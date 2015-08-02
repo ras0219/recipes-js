5b6ae221533cbea1df745d55f0dad1c18635ff3a
Robert Schumacher <ras0219@outlook.com>
2015-08-01 21:23:45 -0700

    save tech levels!

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set('view engine', 'jade')
 
 app.all('/', function (req, res) {
     var cur = {}
+    var query = req.query || {};
     function parseparam(name, qty)
     {
         if (name)
@@ -19,12 +20,12 @@ app.all('/', function (req, res) {
                 cur[name] = Number(qty)
         }
     }
-    parseparam(req.query.i1name, req.query.i1qty)
-    parseparam(req.query.i2name, req.query.i2qty)
-    parseparam(req.query.i3name, req.query.i3qty)
-    parseparam(req.query.i4name, req.query.i4qty)
-    parseparam(req.query.i5name, req.query.i5qty)
-    parseparam(req.query.i6name, req.query.i6qty)
+    parseparam(query.i1name, query.i1qty)
+    parseparam(query.i2name, query.i2qty)
+    parseparam(query.i3name, query.i3qty)
+    parseparam(query.i4name, query.i4qty)
+    parseparam(query.i5name, query.i5qty)
+    parseparam(query.i6name, query.i6qty)
     if (Object.keys(cur).length > 0)
     {
         // do recipes here
@@ -35,10 +36,10 @@ app.all('/', function (req, res) {
         // Evaluate tech level based on submission values
         for (var k in tl)
         {
-            if (k in req.query)
+            if (k in query)
             {
-                tl[k] = Number(req.query[k])
-                console.log("Found tech " + k + " " + req.query[k]);
+                tl[k] = Number(query[k])
+                console.log("Found tech " + k + " " + query[k]);
             }
             else
                 console.log("Didn't find tech " + k);
@@ -52,7 +53,7 @@ app.all('/', function (req, res) {
             begin: begin,
             crafts: crafts.reverse(),
             final: cur,
-            query: req.query,
+            query: query,
             itemlist: itemlist
         })
         console.log("run recipes.")
@@ -64,7 +65,7 @@ app.all('/', function (req, res) {
         res.render('index', {
             basictech: recipes.basictech(),
             techlevel: recipes.techlevel,
-            query: {},
+            query: query,
             itemlist: itemlist
         })
 })
